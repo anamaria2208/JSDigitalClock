7cb8aceab9b12989935466ea6566e41dfdd8af35
Ana-Maria <anamariaolujic2208@gmail.com>
2023-04-28 21:49:02 +0200

    removed unnecessary imports

--- src/script.js
+++ src/script.js
@@ -1,8 +1,7 @@
-import { getHours, getMinutes, getSeconds, format } from "date-fns";
+import { format } from "date-fns";
 
 const clock = document.querySelector(".clock");
 setInterval(() => {
   const date = new Date();
   clock.innerHTML = `${format(date, "HH : mm : ss")}`;
 }, 1000);
-
