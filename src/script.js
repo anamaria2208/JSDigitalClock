158e1fc7dddc4c2af6d4765412c09bd4de99b0d8
Ana-Maria <anamariaolujic2208@gmail.com>
2023-04-28 21:46:55 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import { getHours, getMinutes, getSeconds, format } from "date-fns";
+
+const clock = document.querySelector(".clock");
+setInterval(() => {
+  const date = new Date();
+  clock.innerHTML = `${format(date, "HH : mm : ss")}`;
+}, 1000);
+
